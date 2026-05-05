93078997cf41e976cbc948f5da16ce3a3b911fac
raxwotechnology <your-github-email@example.com>
2026-05-05 23:38:22 +0530

    fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://backend.raxwo.net',
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
       '/uploads': {
-        target: 'https://backend.raxwo.net',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       }
